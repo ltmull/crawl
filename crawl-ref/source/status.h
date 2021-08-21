d7a427ad9d50756d22a2da03446b5b23014cc8b4
Nicholas Feinberg <pleasingfung@gmail.com>
2021-08-21 10:06:38 -0700

    Add a status light for Ignis rF0 (ebering)
    It looks pretty weird if you wouldn't have rF+ anyway, but the
    stated use case was keeping people from wasting !res, so we sort of
    have to show it as long as you don't have rF-...?

--- crawl-ref/source/status.h
+++ crawl-ref/source/status.h
@@ -47,7 +47,8 @@ enum status_type
     STATUS_CURL,
     STATUS_MAXWELLS,
     STATUS_DUEL,
-    STATUS_LAST_STATUS = STATUS_DUEL
+    STATUS_RF_ZERO,
+    STATUS_LAST_STATUS = STATUS_RF_ZERO
 };
 
 struct status_info
